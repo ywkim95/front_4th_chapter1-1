3f8f1b694134f8539f37cf5972cbc4ca9d82d612
KimYoungWoo <rladuddn3924@gmail.com>
2024-12-19 07:57:00 +0900

    FIX: Optional Chaining 추가

--- src/components/post-form.js
+++ src/components/post-form.js
@@ -6,7 +6,7 @@ const PostForm = () => {
     <form id="post-form">
       <div class="mb-4 bg-white rounded-lg shadow p-4">
         <textarea name="content" id="content" class="w-full p-2 border rounded" placeholder="무슨 생각을 하고 계신가요?"></textarea>
-        ${new UserStore().get().username ? UsernameInput() : ""}
+        ${new UserStore().get()?.username ? UsernameInput() : ""}
         <button type="submit" class="mt-2 bg-blue-600 text-white px-4 py-2 rounded">게시</button>
       </div>
     </form>`;
